0ba048b138562199fd540763734334894412b985
Komal Ambagade <126410446+KomalAmbagade@users.noreply.github.com>
2024-03-13 12:57:42 +0530

    updated

--- onChangeEvent/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppClass from './AppClass'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppClass />
-  </React.StrictMode>,
-)
--- todo1/src/Todo.jsx
+++ todo1/src/Todo.jsx
@@ -6,7 +6,7 @@ const Todo = (props) =>
   return (
       <div>
 			<li key={props.index}>
-				{props.task} <button onClick={() => props.remove(i)}>❎</button>
+				{props.task} <button onClick={() => props.remove(props.index)}>❎</button>
 			</li>
 		</div>
 	)
