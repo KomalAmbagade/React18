a3d6391fc9ebcfb473ffaa52f84197354188155c
Komal Ambagade <126410446+KomalAmbagade@users.noreply.github.com>
2024-03-15 09:55:14 +0530

    updated

--- contextAPIAssignment/src/App.jsx
+++ contextAPIAssignment/src/App.jsx
@@ -5,13 +5,20 @@ import Component from './Component'
 import './index.css'
 const App = () => {
 
-  const [mode,setMode]=useState("white")
-  const[textcolor,setTextcolor]=useState("black")
+  const [mode,setMode]=useState("")
+  const[textcolor,setTextcolor]=useState("")
+  const[isDarkMode,setIsDarkMode]=useState(true)
+
+
+  const toggleDarkMode=()=>{
+    setIsDarkMode(prevMode=>!prevMode)
+    isDarkMode?(setMode("black"),setTextcolor("white") ):(setMode("white"),setTextcolor("black"))
+  }
 
   return (
     <div style={{width:'800px',height:'700px',border:'4px solid red',padding:"100px"}}>
      <h2>App Component</h2>
-      <button onClick={()=>{setMode("black") , setTextcolor("white")}}  style={{position:"absolute" ,left:"30px" ,top:"30px"}}>Change the mode</button>
+      <button onClick={toggleDarkMode}  style={{position:"absolute" ,left:"30px" ,top:"30px"}}>{isDarkMode?('light'):('dark')}</button>
       <mainContext.Provider value={{mode,textcolor}}>
         <Component />
 			</mainContext.Provider>
